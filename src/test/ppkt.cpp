dfef8d6bc507aa2938b2f24553d84a50eaea8b73
Kristof Provost <kristof@codepro.be>
2013-06-08 00:05:35 +0200

    Implement very basic ppkt unit tests

--- src/test/ppkt.cpp
+++ src/test/ppkt.cpp
@@ -1,11 +1,29 @@
 #include <CppUTest/TestHarness.h>
+extern "C" {
+#include "ppkt.h"
+};
 
 TEST_GROUP(ppkt)
 {
-
 };
+
+TEST(ppkt, alloc)
+{
+    struct ppkt_t *p = ppkt_alloc(15);
+    CHECK(p);
+    CHECK(15 == ppkt_size(p));
+    ppkt_free(p);
+}
 
 TEST(ppkt, create)
 {
-    CHECK(true);
+    uint8_t buffer[10] = { 0 };
+
+    struct ppkt_t *p = ppkt_create(buffer, sizeof(buffer));
+    CHECK(p);
+    CHECK(10 == ppkt_size(p));
+
+    ppkt_free(p);
+
+    CHECK(buffer[5] == 0);
 }
