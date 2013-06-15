ed34087ac2c04e77bd926d974f0f7504b52bc595
Kristof Provost <kristof@codepro.be>
2013-06-15 23:40:29 +0200

    Turn pnget into a truely generic read tool.

--- src/pnget/pnget.c
+++ src/pnget/pnget.c
@@ -1,37 +1,62 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
 #include "profinet.h"
 
+static void help(const char *name)
+{
+    printf("Usage: %s -a <ip> -d <db> -n <number>\n", name);
+    exit(EXIT_FAILURE);
+}
+
 int main(int argc, char **argv)
 {
-    struct profinet_dev_t *dev = profinet_connect("10.0.3.9");
+    char *ip = NULL;
+    int db = -1;
+    int num = -1;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "a:d:n:h")) != -1)
+    {
+        switch (opt)
+        {
+            case 'a':
+                ip = optarg;
+                break;
+            case 'd':
+                db = atoi(optarg);
+                break;
+            case 'n':
+                num = atoi(optarg);
+                break;
+            case 'h':
+            default:
+                help(argv[0]);
+        }
+    }
+
+    if (! ip || db == -1 || num == -1)
+        help(argv[0]);
+
+    struct profinet_dev_t *dev = profinet_connect(ip);
     if (! dev)
     {
         printf("Failed to connect: %s (%d)\n", strerror(errno), errno);
         return 1;
     }
 
-    uint16_t temp = 0;
-    err_t err = profinet_read_word(dev, 10, 4, &temp);
+    uint16_t value = 0;
+    err_t err = profinet_read_word(dev, db, num, &value);
     if (! OK(err))
     {
-        printf("Failed to read temperature\n");
+        printf("Failed to read\n");
         goto exit;
     }
 
-    printf("Temperature: %f\n", ((int16_t)temp) / 10.0);
-
-    uint16_t setpoint = 0;
-    err = profinet_read_word(dev, 10, 6, &setpoint);
-    if (! OK(err))
-    {
-        printf("Failed to read temperature\n");
-        goto exit;
-    }
-    printf("Temperature setpoint: %f\n", ((int16_t)setpoint) / 10.0);
+    printf("Value: 0x%04x\n", value);
 
 exit:
     profinet_disconnect(dev);
